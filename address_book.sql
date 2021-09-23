f528d27a4c4eae3fcd4ac4031fc3fde61a32d723
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-24 02:17:09 +0530

    ADDED: Insert new contacts to address book table

--- address_book.sql
+++ address_book.sql
@@ -4,3 +4,9 @@ SELECT DATABASE();
 
 CREATE TABLE address_book(id INT unsigned NOT NULL AUTO_INCREMENT, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, address VARCHAR(200), city VARCHAR(30), state VARCHAR(30), zip INT unsigned, phone_number LONG, email VARCHAR(320), PRIMARY KEY (id));
 DESCRIBE address_book;
+
+INSERT INTO address_book VALUE(1,"Rajat","S P","2nd Cross, HosaRoad","Bangalore","KARNATAKA",560100,8934267543,"raj@gmail.com");
+SELECT * FROM address_book;
+INSERT INTO address_book (first_name, last_name, address, city, state, zip, phone_number, email) VALUES ("Pradeep","Nayar","2nd Cross, Yelahanka","Bangalore","KARNATAKA",562130,8434227543,"pradeep@gmail.com"),("Anjali","Verma","5th Cross, Malleshwaram","Bangalore","Karnataka",730160,8937126754,"anjaliverma@gmail.com"),("Hemanth","Kumar","2nd Street, Andheri","Mumbai","Maharashtra",530160,8937856774,"hemanthk@gmail.com");
+SELECT * FROM address_book;
+
