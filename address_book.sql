b2bb3c13a66874eb797e3573ada1c608b108d7c1
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-24 02:14:41 +0530

    ADDED: Create a table named address book in address book service database

--- address_book.sql
+++ address_book.sql
@@ -1,3 +1,6 @@
 CREATE DATABASE  address_book;
 use address_book_service
 SELECT DATABASE();
+
+CREATE TABLE address_book(id INT unsigned NOT NULL AUTO_INCREMENT, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, address VARCHAR(200), city VARCHAR(30), state VARCHAR(30), zip INT unsigned, phone_number LONG, email VARCHAR(320), PRIMARY KEY (id));
+DESCRIBE address_book;
