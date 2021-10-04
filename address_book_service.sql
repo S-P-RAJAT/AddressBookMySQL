a28ef23592df8f79c7028f1d5c9c55bf3f381244
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-04 23:43:33 +0530

    MODIFIED: ER model

--- address_book_service.sql
+++ address_book_service.sql
@@ -18,10 +18,6 @@
 --
 -- Table structure for table `address`
 --
-DROP DATABASE IF EXISTS `address_book_service`;
-
-CREATE DATABASE `address_book_service`;
-USE `address_book_service`
 
 DROP TABLE IF EXISTS `address`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -36,7 +32,7 @@ CREATE TABLE `address` (
   PRIMARY KEY (`address_id`),
   KEY `contact_id` (`contact_id`),
   CONSTRAINT `address_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `contact` (`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +41,7 @@ CREATE TABLE `address` (
 
 LOCK TABLES `address` WRITE;
 /*!40000 ALTER TABLE `address` DISABLE KEYS */;
-INSERT INTO `address` VALUES (1,1,'2nd Cross, HosaRoad','Bangalore','KARNATAKA',560100),(2,2,'2nd Street, Andheri','Mumbai','Maharashtra',530160),(3,3,'5th Cross, Malleshwaram','Bangalore','Karnataka',730160);
+INSERT INTO `address` VALUES (1,1,'2nd Cross, HosaRoad','Bangalore','KARNATAKA',560100),(2,2,'2nd Street, Andheri','Mumbai','Maharashtra',530160),(3,3,'2nd Cross, Malleshwaram','Bangalore','Karnataka',543923);
 /*!40000 ALTER TABLE `address` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,6 +55,7 @@ DROP TABLE IF EXISTS `address_book`;
 CREATE TABLE `address_book` (
   `book_id` int unsigned NOT NULL AUTO_INCREMENT,
   `book_name` varchar(50) NOT NULL,
+  `book_type` varchar(50) NOT NULL,
   PRIMARY KEY (`book_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -69,7 +66,7 @@ CREATE TABLE `address_book` (
 
 LOCK TABLES `address_book` WRITE;
 /*!40000 ALTER TABLE `address_book` DISABLE KEYS */;
-INSERT INTO `address_book` VALUES (1,'Book1'),(2,'Book2');
+INSERT INTO `address_book` VALUES (1,'Book1','family'),(2,'Book2','friends');
 /*!40000 ALTER TABLE `address_book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,15 +81,12 @@ CREATE TABLE `contact` (
   `contact_id` int unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
-  `phone_number` bigint DEFAULT NULL,
+  `phone_number` bigint NOT NULL,
   `email` varchar(320) DEFAULT NULL,
-  `book_id` int unsigned DEFAULT NULL,
   `date_added` date DEFAULT NULL,
   PRIMARY KEY (`contact_id`),
-  UNIQUE KEY `no_duplicate_in_book` (`first_name`,`last_name`,`book_id`),
-  KEY `book_id` (`book_id`),
-  CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `address_book` (`book_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `phone_number` (`phone_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,59 +95,35 @@ CREATE TABLE `contact` (
 
 LOCK TABLES `contact` WRITE;
 /*!40000 ALTER TABLE `contact` DISABLE KEYS */;
-INSERT INTO `contact` VALUES (1,'Rajat','S P',9835647328,'raj@gmail.com',1,'2021-09-21'),(2,'Hemanth','Kumar',8937856774,'hemanthk@gmail.com',2,'2021-09-25'),(3,'Anjali','Verma',8937126754,'anjaliverma@gmail.com',1,'2021-09-28');
+INSERT INTO `contact` VALUES (1,'Rajat','S P',9835647328,'raj@gmail.com','2021-09-21'),(2,'Hemanth','Kumar',8937856774,'hemanthk@gmail.com','2021-09-25'),(3,'Anjali','Verma',8937126754,'anjaliverma@gmail.com','2021-09-28'),(15,'Pradeep','Shennoy',8223427865,'ankitsharma@gmail.com','2021-10-04');
 /*!40000 ALTER TABLE `contact` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `contact_type`
+-- Table structure for table `contact_of_book`
 --
 
-DROP TABLE IF EXISTS `contact_type`;
+DROP TABLE IF EXISTS `contact_of_book`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `contact_type` (
+CREATE TABLE `contact_of_book` (
   `contact_id` int unsigned NOT NULL,
-  `type_id` int unsigned NOT NULL,
-  PRIMARY KEY (`contact_id`,`type_id`),
-  KEY `type_id` (`type_id`),
-  CONSTRAINT `contact_type_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `contact` (`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `contact_type_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `address_book_type` (`type_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `book_id` int unsigned NOT NULL,
+  PRIMARY KEY (`contact_id`,`book_id`),
+  KEY `book_id` (`book_id`),
+  CONSTRAINT `contact_of_book_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `contact` (`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `contact_of_book_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `address_book` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `contact_type`
+-- Dumping data for table `contact_of_book`
 --
 
-LOCK TABLES `contact_type` WRITE;
-/*!40000 ALTER TABLE `contact_type` DISABLE KEYS */;
-INSERT INTO `contact_type` VALUES (1,1),(1,2),(3,2),(2,3);
-/*!40000 ALTER TABLE `contact_type` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `address_book_type`
---
-
-DROP TABLE IF EXISTS `address_book_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `address_book_type` (
-  `type_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `address_book_type`
---
-
-LOCK TABLES `address_book_type` WRITE;
-/*!40000 ALTER TABLE `address_book_type` DISABLE KEYS */;
-INSERT INTO `address_book_type` VALUES (1,'Family'),(2,'Friends'),(3,'Profession');
-/*!40000 ALTER TABLE `address_book_type` ENABLE KEYS */;
+LOCK TABLES `contact_of_book` WRITE;
+/*!40000 ALTER TABLE `contact_of_book` DISABLE KEYS */;
+INSERT INTO `contact_of_book` VALUES (1,1),(3,1),(1,2),(2,2),(15,2);
+/*!40000 ALTER TABLE `contact_of_book` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -165,4 +135,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-01  3:51:43
+-- Dump completed on 2021-10-05  1:41:55
